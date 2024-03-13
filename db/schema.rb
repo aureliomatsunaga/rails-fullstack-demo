1f1d0375c33a3072c39c41568ce610ac021f0e7c
aureliomatsunaga <aureliomatsunaga@gmail.com>
2024-03-13 19:28:31 -0300

    feat: added posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_13_214620) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_13_221758) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "posts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
