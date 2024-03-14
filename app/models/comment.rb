2623aa48b6d2ab542cd7efee352b74c0dce64022
aureliomatsunaga <aureliomatsunaga@gmail.com>
2024-03-14 18:49:10 -0300

    feat: added comments on posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  broadcasts_to :post
+  validates_presence_of :content
 end
--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_13_225457) do
 
   create_table "comments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "post_id", null: false
-    t.string "user_name"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
